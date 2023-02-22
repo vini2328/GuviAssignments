193d6be34be65e95316c3e2ebe657ccd45eac6a0
vini2328 <123352754+vini2328@users.noreply.github.com>
2023-02-22 16:23:58 +0530

    Callbackhelltaskday17_Guvi

--- script.js
+++ script.js
@@ -1,81 +1,36 @@
-let dataAll = [];
-// example {id:1592304983049, title: 'Deadpool', year: 2015}
-const addToTable = (ev)=>{
-    try{
-        let food = [];
-        ev.preventDefault();  //to stop the form submitting
-        var markedCheckbox = document.querySelectorAll('input[type="checkbox"]:checked');
-        for (var checkbox of markedCheckbox) {
-
-        food.push(checkbox.value)
-        }
-        
-        console.log(food.length)
-        let dataX = {
-            firstName: document.getElementById('first-name').value,
-            lastName: document.getElementById('last-name').value,
-            address: document.getElementById('address').value,
-            pincode: document.getElementById('pincode').value,
-            gender: document.querySelector('input[type="radio"]:checked').value,
-            foodx: food,
-            state: document.getElementById('state').value,
-            country: document.getElementById('pincode').value
-        }
-        if (dataX.firstName  == "") {  //validation for first name
-            alert("First Name must be filled out");
-            return false;
-        }
-
-        // document.forms[0].reset(); // to clear the form for the next entries
-        
-
-        //for display purposes only
-        
-        // let pre = document.querySelector('#msg pre');
-        // pre.textContent = '\n' + JSON.stringify(movies, '\t', 2);
-        if(food.length > 1){  // to check if atleat two food items are selected
-            dataAll.push(dataX); 
-            console.warn('added' , {dataAll} );
-            document.querySelector('form').reset();  // to clear the form for the next entries
-            let pre = document.querySelector(".Content")
-            pre.innerHTML=`<tr><th>First Name</th>
-                <th>Last Name</th>
-                <th>Address</th>
-                <th>Pincode</th>
-                <th>Gender</th>
-                <th>Food</th>
-                <th>State</th>
-                <th>Country</th>
-                </tr>`;
-            for(let i=0;i<dataAll.length;i++){
-                pre.innerHTML += `<tr>
-                <td>${dataAll[i].firstName}</th>
-                <td>${dataAll[i].lastName}</th>
-                <td>${dataAll[i].address}</th>
-                <td>${dataAll[i].pincode}</th>
-                <td>${dataAll[i].gender}</th>
-                <td>${dataAll[i].foodx}</th>
-                <td>${dataAll[i].state}</th>
-                <td>${dataAll[i].country}</th>
-                </tr>`
-            }
-            window.alert('Form submitted successfully')
-        }
-        else{
-            window.alert('Select atleat 2 food items')
-        }
-            
-        //saving to localStorage
-        localStorage.setItem('MyUserList', JSON.stringify(dataAll) );
-        
-       
-    }
-        catch(err){
-            window.alert('Fill all the details in form \n'+err)
-        }
-
-        
-}
-    document.addEventListener('DOMContentLoaded', ()=>{
-    document.getElementById('btn').addEventListener('click', addToTable);
-});
+setTimeout(()=>{
+    console.log(10)
+    document.body.innerHTML=`<<h1>10</h1>`
+    setTimeout(() => {
+        console.log(9)
+        document.body.innerHTML=`<h1>9</h1>`
+        setTimeout(() => {
+            document.body.innerHTML=`<h1>8</h1>`
+            setTimeout(() => {
+                document.body.innerHTML=`<h1>7</h1>`
+                setTimeout(() => {
+                    document.body.innerHTML=`<h1>6</h1>`
+                    setTimeout(() => {
+                        document.body.innerHTML=`<h1>5</h1>`
+                        setTimeout(() => {
+                            document.body.innerHTML=`<h1>4</h1>`
+                            setTimeout(() => {
+                                document.body.innerHTML=`<h1>3</h1>`
+                                setTimeout(() => {
+                                    document.body.innerHTML=`<h1>2</h1>`
+                                    setTimeout(() => {
+                                        document.body.innerHTML=`<h1>1</h1>`
+                                        setTimeout(() => {
+                                            document.body.innerHTML=`<h1>Happy Independence Day!!🧡</h1>`
+                                           
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
