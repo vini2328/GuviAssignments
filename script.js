bcd88431d82624a91731788471462a0fe2ed9434
vini2328 <123352754+vini2328@users.noreply.github.com>
2023-02-25 16:38:10 +0530

    Weather App

--- script.js
+++ script.js
@@ -1,36 +1,34 @@
-setTimeout(()=>{
-    console.log(10)
-    document.body.innerHTML=`<<h1>10</h1>`
-    setTimeout(() => {
-        console.log(9)
-        document.body.innerHTML=`<h1>9</h1>`
-        setTimeout(() => {
-            document.body.innerHTML=`<h1>8</h1>`
-            setTimeout(() => {
-                document.body.innerHTML=`<h1>7</h1>`
-                setTimeout(() => {
-                    document.body.innerHTML=`<h1>6</h1>`
-                    setTimeout(() => {
-                        document.body.innerHTML=`<h1>5</h1>`
-                        setTimeout(() => {
-                            document.body.innerHTML=`<h1>4</h1>`
-                            setTimeout(() => {
-                                document.body.innerHTML=`<h1>3</h1>`
-                                setTimeout(() => {
-                                    document.body.innerHTML=`<h1>2</h1>`
-                                    setTimeout(() => {
-                                        document.body.innerHTML=`<h1>1</h1>`
-                                        setTimeout(() => {
-                                            document.body.innerHTML=`<h1>Happy Independence Day!!🧡</h1>`
-                                           
-                                        }, 1000);
-                                    }, 1000);
-                                }, 1000);
-                            }, 1000);
-                        }, 1000);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}, 1000);
+var Weathercontainer=document.getElementById("Weathercontainer")
+var Cityname=document.getElementById("Cityname")
+
+let apikey="f1dcc6f5ef0edd3a68267cb517ec20aa"
+
+async function Getweatherdata(){
+    var Cityname=document.getElementById("Cityname").value
+    console.log(Cityname)
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${Cityname}&appid=${apikey}`
+    let data=await fetch(url)
+    let res=await data.json()
+    console.log(res)
+
+    Weathercontainer.innerHTML=`
+    <div class="row g-0">
+    <div class="col-md-4">
+      <img src="https://cdn-icons-png.flaticon.com/512/3845/3845731.png" class="img-fluid rounded-startmp-0" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title m-0 mb-2 "><b>City name- ${Cityname}</b></h5>
+        <p class="card-text m-0"><small class="text-muted">Temperature- ${res.main.temp}°K</small></p>
+        <p class="card-text m-0"><small class="text-muted">feels_like- ${res.main.feels_like}</small></p>
+        <p class="card-text m-0"><small class="text-muted">Description- ${res.weather[0].description}</small></p>
+        <p class="card-text m-0"><small class="text-muted">Degree- ${res.wind.deg}°</small></p>
+        <p class="card-text m-0"><small class="text-muted">Humidity- ${res.main.humidity}</small></p>
+
+      </div>
+      </div>
+      </div>
+`
+
+
+}Getweatherdata()
