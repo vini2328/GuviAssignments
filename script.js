3cf9f3870dd983c4cb8c0beb80d67a472359081f
vini2328 <123352754+vini2328@users.noreply.github.com>
2023-02-26 21:25:16 +0530

    day18 task-restcountries weather app

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
-var Weathercontainer=document.getElementById("Weathercontainer")
-var Cityname=document.getElementById("Cityname")
-
-let apikey="f1dcc6f5ef0edd3a68267cb517ec20aa"
-
-async function Getweatherdata(){
-    var Cityname=document.getElementById("Cityname").value
-    console.log(Cityname)
-    let url=`https://api.openweathermap.org/data/2.5/weather?q=${Cityname}&appid=${apikey}`
-    let data=await fetch(url)
+async function Getdata(){
+    let data=await fetch("https://restcountries.com/v3.1/all")
     let res=await data.json()
-    console.log(res)
+    let imgcard=document.getElementsByClassName("Imagecard")
+    var item=null
+    let doit=false
+    for(let i=0;i<res.length;i++){
+        console.log(res[i])
+        item = res[i].name.common
+        imgcard[0].innerHTML+=`
+        <div class="bg-dark text-white my-4 mx-4 text-center"style="width: 18rem; height:50%">
+            <span>${res[i].name.common}</span>
+            <div class="card"  style="width: 18rem ">
+                <img class="card-img-top "style="height: 160px" src=${res[i].flags.svg} alt="Card image cap" />
+                    <div class="card-body text-light bg-dark ">
+                        <h6 class="card-title">Capital: ${res[i].capital[0]}</h6>
+                        <h6 class="card-text">Region : ${res[i].region}</h6>
+                        <h6 class="card-title">Country Code: ${res[i].cca3}</h6> 
+                        <button onClick="Getweatherdata('${res[i].name.common}')" class="btn btn-secondary">Click for Weather</button>
+                     </div>
+            </div>
+        </div>`
+
+        
+    }
+}Getdata()
+
+async function Getweatherdata(x){ 
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${x}&appid=f1dcc6f5ef0edd3a68267cb517ec20aa`
+    let data1= await fetch(url)
+    let res=await data1.json()
+    var Weathercontainer=document.getElementById("Weathercontainer")
+    //
 
     Weathercontainer.innerHTML=`
+    <div class="container">
     <div class="row g-0">
     <div class="col-md-4">
       <img src="https://cdn-icons-png.flaticon.com/512/3845/3845731.png" class="img-fluid rounded-startmp-0" alt="...">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title m-0 mb-2 "><b>City name- ${Cityname}</b></h5>
+        <h5 class="card-title m-0 mb-2 "><b>City name- ${x}</b></h5>
         <p class="card-text m-0"><small class="text-muted">Temperature- ${res.main.temp}°K</small></p>
         <p class="card-text m-0"><small class="text-muted">feels_like- ${res.main.feels_like}</small></p>
         <p class="card-text m-0"><small class="text-muted">Description- ${res.weather[0].description}</small></p>
@@ -28,7 +50,10 @@ async function Getweatherdata(){
       </div>
       </div>
       </div>
+      </div>
 `
 
+    
+    
 
-}Getweatherdata()
+}
